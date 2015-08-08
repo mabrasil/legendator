6a0d156160d7235e7fa971c25f004fe80c6802a0
pogist <murilo.paixao.2@gmail.com>
2015-08-08 11:29:05 -0300

    Fix subtitle method

--- src/legendator.rb
+++ src/legendator.rb
@@ -1,7 +1,7 @@
 module Legendator
   class File
     def initialize(file)
-      @file = ::File.open(::File.expand_path(file),"r")
+      @file = ::File.open(file,"r")
       #fix the file encode to remove the InvalidArgument error
       @file_content = IO.read(@file).force_encoding("ISO-8859-1").encode("utf-8",replace: nil)
       @subtitle_regex =  /(\d+)\s+(\d+:\d+:\d+,\d+)\s+-->\s+(\d+:\d+:\d+,\d+)\s+(.+)/
@@ -15,6 +15,8 @@ module Legendator
         matched ||= match if match[0] == position.to_s 
       end
 			
+      return nil if matched.nil?
+
       subtitle = {position: matched[0].to_i,starts_at: matched[1],ends_at: matched[2],text: matched[3]}
     end
   end
